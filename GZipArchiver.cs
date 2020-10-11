a991b2bf8c5d633654fa1b48bd60c34c9eefa1e4
Aidar Akhmetshin <aidar.akhmetshin@netcracker.com>
2020-10-11 19:07:30 +0300

    Create basic GZip archiver

---
+++ GZipArchiver.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+
+namespace GZipTest
+{
+    class GZipArchiver
+    {
+        public static void Compress(FileInfo sourceFile)
+        {
+            using (FileStream sourceFileStream = sourceFile.OpenRead())
+            {
+                using (FileStream compressedFile = File.Create(sourceFile.FullName + ".gzip"))
+                {
+                    using (GZipStream compressionStream = new GZipStream(compressedFile, CompressionMode.Compress))
+                    {
+                        sourceFileStream.CopyTo(compressionStream);
+                    }
+                }
+
+                FileInfo info = new FileInfo(sourceFile.Name + ".gzip");
+                Console.WriteLine($"Compressed {sourceFile.Name} from {sourceFile.Length.ToString()} to {info.Length.ToString()} bytes.");
+            }
+        }
+
+        public static void Decompress(FileInfo zipFile)
+        {
+            using (FileStream zipFileStream = zipFile.OpenRead())
+            {
+                string zipFileName = zipFile.FullName;
+                string unzippedFileName = zipFileName.Remove(zipFileName.Length - zipFile.Extension.Length);
+
+                using (FileStream unpackedFile = File.Create(unzippedFileName))
+                {
+                    using (GZipStream decompressionStream = new GZipStream(zipFileStream, CompressionMode.Decompress))
+                    {
+                        decompressionStream.CopyTo(unpackedFile);
+                        Console.WriteLine($"File {zipFile.Name} decompressed");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace GZipTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = args[0];
+            FileInfo sourceFile = new FileInfo(args[1]);
+
+            if ("compress".Equals(args[0]))
+                GZipArchiver.Compress(sourceFile);
+            else if ("decompress".Equals(args[0]))
+                GZipArchiver.Decompress(sourceFile);
+        }
+    }
+}
